ea2ce42017e1368961514dcbcd73acea6cd5a20e
meysam tajik <meysamtajik1376@gmail.com>
2024-01-13 22:20:45 +0330

    add manager

--- core/models.py
+++ core/models.py
@@ -1,10 +1,50 @@
 from django.db import models
 
+
+class LogicalQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(is_deleted=True)
+
+    def hard_delete(self):
+        return super().delete()
+
+
+class LogicalManager(models.Manager):
+
+    def get_queryset(self):
+        return LogicalQuerySet(self.model).filter(is_deleted=False, is_active=True)
+
+    def archive(self):
+        return LogicalQuerySet(self.model)
+
+    def deleted(self):
+        return LogicalQuerySet(self.model).filter(is_deleted=True)
+
+
 class BaseModel(models.Model):
-    created_at = models.DateField(auto_now_add = True)
-    updated_at = models.DateField(auto_now = True)
-    is_active = models.BooleanField(default = True)
-    is_deleted = models.BooleanField(default = False)
-    delete_at = models.DateField(null = True, blank = True)
-    class Meta :
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    delete_at = models.DateField(null=True, blank=True)
+
+    objects = LogicalManager()
+
+    class Meta:
         abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
+
+    def undelete(self):
+        self.is_deleted = False
+        self.save()
+
+class StatusMixin:
+    @property
+    def status(self) -> bool:
+        return self.is_active and not self.is_deleted  # noqa
--- product/models.py
+++ product/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
-from core.models import BaseModel
-from django.core.exceptions import ValidationError      
-from account.models import CustomUser  
+from core.models import BaseModel, StatusMixin
+from account.models import CustomUser
 from django.core.exceptions import ValidationError
 
-class Category(BaseModel):
-    category_name = models.CharField(max_length= 255)
-    category = models.ForeignKey('self',on_delete = models.CASCADE, related_name = "categories", blank=True, null = True)
+
+class Category(BaseModel, StatusMixin):
+    category_name = models.CharField(max_length=255)
+    category = models.ForeignKey('self', on_delete=models.CASCADE, related_name="categories", blank=True, null=True)
     slug = models.SlugField(unique=True, blank=True, max_length=255)
-    image = models.ImageField(upload_to="categories",null=True,blank=True)
+    image = models.ImageField(upload_to="categories", null=True, blank=True)
+
     class Meta:
-        ordering = ("category_name"  ,)
-        verbose_name_plural= "categories"
+        ordering = ("category_name",)
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.category_name
@@ -23,30 +24,35 @@ class Category(BaseModel):
             self.slug = slugify(self.category_name)
         super().save(*args, **kwargs)
 
-class Product(BaseModel):
-    category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name = "products")
-    discount = models.ForeignKey('Discount', on_delete = models.CASCADE, related_name = "products",blank=True, null = True)
+
+class Product(BaseModel, StatusMixin):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    discount = models.ForeignKey('Discount', on_delete=models.CASCADE, related_name="products", blank=True, null=True)
     price = models.PositiveIntegerField()
-    mojoudi = models.PositiveIntegerField()
-    item_name = models.CharField(max_length= 255)
-    brand = models.CharField(max_length= 255)
+    inventory = models.PositiveIntegerField()
+    item_name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True, blank=True, max_length=255)
     image = models.ImageField(upload_to="products")
+
     class Meta:
-        ordering = ("-id"  ,)
+        ordering = ("-id",)
+
     def clean(self):
-        if self.discount :
-            if self.discount.type == "number" and self.discount.amount> self.price:
-                raise ValidationError({'discount':('برای تخفیف عددی نباید مقدار  تخفیف بیشتر از مبلغ محصول باشد')})
+        if self.discount:
+            if self.discount.type == "number" and self.discount.amount > self.price:
+                raise ValidationError({'discount': ('برای تخفیف عددی نباید مقدار  تخفیف بیشتر از مبلغ محصول باشد')})
+
     def __str__(self):
         return self.item_name
-    
+
     def like_count(self):
         return self.likes.count()
+
     def is_like(self, user):
-        like = user.likes.filter(product = self).exists()
-        if like :
+        like = user.likes.filter(product=self).exists()
+        if like:
             return True
         else:
             return False
@@ -57,57 +63,70 @@ class Product(BaseModel):
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse("home:detail", args = [self.slug])
-    
-class Discount(BaseModel):
+        return reverse("home:detail", args=[self.slug])
+
+
+class Discount(models.Model):
     TYPE_PERCENT = "percent"
-    TYPE_NUMBER= "number"
+    TYPE_NUMBER = "number"
     TYPE_SELECT = (
-        (TYPE_PERCENT,'PERCENT'),
-        (TYPE_NUMBER,'NUMBER')
+        (TYPE_PERCENT, 'PERCENT'),
+        (TYPE_NUMBER, 'NUMBER')
     )
+
     def max_diss_check(value):
-        if type=="number" and value !=0.0:
+        if type == "number" and value != 0.0:
             raise ValidationError(
-               ("%(value)s برای تخفیف های درصدی میبایست این مقدار 0.0 باشد"),
+                ("%(value)s برای تخفیف های درصدی میبایست این مقدار 0.0 باشد"),
                 params={"value": value},
             )
+
     def amount_check(value):
-        if type=="percent" and value >100:
+        if type == "percent" and value > 100:
             print("meysam")
             raise ValidationError(
-               ("%(value)s برای تخفیف درصدی نباید مقدار درصد بیشتر از 100 باشد"),
+                ("%(value)s برای تخفیف درصدی نباید مقدار درصد بیشتر از 100 باشد"),
                 params={"value": value},
 
             )
-    type = models.CharField(max_length=10,choices=TYPE_SELECT, default=TYPE_PERCENT)
-    max_dis= models.PositiveIntegerField( null = True, blank = True)
-    discount_code = models.CharField(max_length= 20, null = True, blank = True)
+
+    type = models.CharField(max_length=10, choices=TYPE_SELECT, default=TYPE_PERCENT)
+    max_dis = models.PositiveIntegerField(null=True, blank=True)
+    discount_code = models.CharField(max_length=20, null=True, blank=True)
     amount = models.PositiveIntegerField()
+
     def __str__(self):
         return f'type --> {self.type} amount --> {self.amount}'
-    
+
     def clean(self):
-        if self.type=="percent" and self.amount >100:
-            raise ValidationError({'amount':('برای تخفیف درصدی نباید مقدار درصد بیشتر از 100 باشد')})
-        elif self.type=="number" and self.max_dis:
-             raise ValidationError({'max_dis':('  برای مقادیر عددی این فیلد میبایست خالی باشد')})
-    
-class Like(BaseModel):
-    user = models.ForeignKey(CustomUser,on_delete = models.CASCADE, related_name = "likes")
-    product = models.ForeignKey(Product,on_delete = models.CASCADE, related_name = "likes")
+        if self.type == "percent" and self.amount > 100:
+            raise ValidationError({'amount': ('برای تخفیف درصدی نباید مقدار درصد بیشتر از 100 باشد')})
+        elif self.type == "number" and self.max_dis:
+            raise ValidationError({'max_dis': ('  برای مقادیر عددی این فیلد میبایست خالی باشد')})
+
+
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="likes")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="likes")
+
     def __str__(self):
         return f'{self.user} liked {self.product}'
-    
+
+    def clean(self):
+        can = Like.objects.filter(user=self.user, product = self.product).exists()
+        if can:
+            raise ValidationError({'user': (' یک بار با این کاربری لایک انجام شده است')})
+
 
 class Comment(BaseModel):
-    user =models.ForeignKey(CustomUser, on_delete=models.CASCADE ,related_name = 'comments')
-    product =models.ForeignKey(Product, on_delete=models.CASCADE ,related_name = 'comments')
-    reply = models.ForeignKey('self', on_delete=models.CASCADE ,related_name = 'comments' ,blank=True, null = True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='comments', blank=True, null=True)
     is_reply = models.BooleanField(default=False)
     body = models.TextField()
 
     class Meta:
-        ordering = ("-id"  ,)
+        ordering = ("-id",)
+
     def __str__(self) -> str:
         return f'{self.user.username} comment for {self.product.item_name}'
