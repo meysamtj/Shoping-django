4aadc20c321f5772de4719802c485ce3edf75802
meysam tajik <meysamtajik1376@gmail.com>
2024-01-20 22:12:18 +0330

    update html product and delete before product and use paginator in views

--- product/views.py
+++ product/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, RedirectView, ListView, DetailView
 from .models import Product, Image
 from orders.models import OrderItem
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 
 class Products(ListView):
-    template_name = 'product/product.html'
+    template_name = 'product/product2.html'
     model = Product  # object Product.objects.filter(id=pk)
     context_object_name = 'products_all'
+    paginate_by = 6
 
     def get_queryset(self):
+        # paginator = Paginator(Product.objects.filter(category__id=self.kwargs['pk']), 1)
+        # page = self.request.GET.get('page')
+        # profile = paginator.get_page(page)  # data
         return Product.objects.filter(category__id=self.kwargs['pk'])
 
     def get_context_data(self, **kwargs):
@@ -24,7 +29,7 @@ class Products(ListView):
 
 
 class DetailProduct(ListView):
-    template_name = 'product/detail2.html'
+    template_name = 'product/detail.html'
     model = Product  # object Car.objects.filter(id=pk)
     slug_field = 'slug'
     context_object_name = 'product'
@@ -36,5 +41,6 @@ class DetailProduct(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["images"] = Image.objects.filter(product=self.get_queryset())
-        context["relateds"] = Product.objects.filter(category=self.get_queryset().category).exclude(id=self.get_queryset().id)
+        context["relateds"] = Product.objects.filter(category=self.get_queryset().category).exclude(
+            id=self.get_queryset().id)
         return context
