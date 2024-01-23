bb76337fca12f5b2a3794b84d7d8473b7b1fd0a5
meysam tajik <meysamtajik1376@gmail.com>
2024-01-23 18:44:10 +0330

    add  class redis.py for otp and next

---
+++ redis_otp.py
@@ -0,0 +1,31 @@
+import redis
+
+
+class saveRedisotp:
+    redis_host = 'localhost'
+    redis_port = 6379
+    redis_pass = ''
+    redis_db = 0
+    rd = redis.Redis(host=redis_host, port=redis_port, password=redis_pass, db=redis_db)
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def set_redis(cls, key, value):
+        cls.rd.set(key, value)
+
+
+    def my_delete(cls, key):
+        cls.rd.delete(key)
+
+    @classmethod
+    def get_redis(cls, key):
+        return cls.rd.get(key)
+
+
+
+if __name__ == '__main__':
+    redis = ghazanfar()
+    redis.set_redis("famliy", "tajik")
+    redis.my_delete("num")
